5c64ceb760136897e7adc9a0c6609dbfca8f7d86
erlingjd <16976922+erlingjd@users.noreply.github.com>
2024-12-03 12:25:25 +0100

    Revert "Logger mer for debugging"
    This reverts commit dda13c7488f267369d3298d6c7cacc8ef0f3f53f.

--- src/main/kotlin/no/nav/hjelpemidler/oppgave/listener/kafka/KafkaStreamsExtensions.kt
+++ src/main/kotlin/no/nav/hjelpemidler/oppgave/listener/kafka/KafkaStreamsExtensions.kt
@@ -1,6 +1,5 @@
 package no.nav.hjelpemidler.oppgave.listener.kafka
 
-import io.github.oshai.kotlinlogging.KotlinLogging
 import no.nav.hjelpemidler.configuration.Environment
 import no.nav.hjelpemidler.oppgave.listener.Configuration
 import org.apache.kafka.streams.KafkaStreams
@@ -9,8 +8,6 @@ import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.Topology
 import org.apache.kafka.streams.kstream.KStream
 import org.apache.kafka.streams.kstream.Produced
-
-val tempLog = KotlinLogging.logger {}
 
 fun topology(block: StreamsBuilder.() -> Unit): Topology = StreamsBuilder().apply(block).build()
 
@@ -23,9 +20,6 @@ infix fun <K, V> K.withValue(value: V): KeyValue<K, V> = KeyValue.pair(this, val
 
 inline fun <reified T> KStream<String, T>.toRapid() {
     if (!Environment.current.isProd) { // TODO: fjernes ved prodsetting
-        peek { key, oppgaveEvent ->
-            tempLog.info { "Sendte melding om oppgaveId til rapid: $key" }
-        }
-            .to(Configuration.KAFKA_RAPID_TOPIC, Produced.with(stringSerde, jsonSerde<T>()))
+        to(Configuration.KAFKA_RAPID_TOPIC, Produced.with(stringSerde, jsonSerde<T>()))
     }
 }
