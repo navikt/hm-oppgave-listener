fc9bbeb8ca2361e3017f4f13f3af1c082ea2b285
Ole Steinar Lillestøl Skrede <ole.steinar.lillestol.skrede@nav.no>
2025-06-20 16:01:58 +0200

    Fjern testlog

--- src/main/kotlin/no/nav/hjelpemidler/oppgave/listener/Application.kt
+++ src/main/kotlin/no/nav/hjelpemidler/oppgave/listener/Application.kt
@@ -1,6 +1,5 @@
 package no.nav.hjelpemidler.oppgave.listener
 
-import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
@@ -12,7 +11,6 @@ import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 import io.micrometer.prometheusmetrics.PrometheusConfig
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
-import no.nav.hjelpemidler.logging.teamInfo
 import no.nav.hjelpemidler.oppgave.listener.kafka.KafkaStreamsPlugin
 import no.nav.hjelpemidler.oppgave.listener.kafka.kafkaStreams
 import no.nav.hjelpemidler.oppgave.listener.oppgave.oppgavehendelse
@@ -21,12 +19,7 @@ fun main() {
     embeddedServer(Netty, Configuration.HTTP_PORT, module = Application::main).start()
 }
 
-private val log = KotlinLogging.logger { }
-
 fun Application.main() {
-
-    log.info { "TEST_LOG" }
-    log.teamInfo { "TEST_TEAM_LOGS" }
 
     val meterRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
     install(MicrometerMetrics) {
